fdbdbf43a56ebf4d59a51e6c9891c79208aa484b
Artashes Sargsyan <41629822+Arto1993@users.noreply.github.com>
2022-09-21 20:31:50 +0400

    Initial commit

---
+++ src/components/SlateEditor/Toolbar/toolbarGroups.js
@@ -0,0 +1,23 @@
+const toolbarGroups = [
+  [
+    {
+      id: 3,
+      format: "bold",
+      type: "mark"
+    },
+    {
+      id: 6,
+      format: "strikethrough",
+      type: "mark"
+    }
+  ],
+  [
+    {
+      id: 20,
+      format: "link",
+      type: "link"
+    }
+  ]
+];
+
+export default toolbarGroups;
